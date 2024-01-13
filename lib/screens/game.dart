5328597b18a207b0f913b119a967a3162eb9fc85
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-13 22:04:08 +0100

    commit

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -159,7 +159,7 @@ class _GameScreenState extends State<GameScreen> {
             ),
           ),
           const Spacer(),
-          Center(child: I,)
+          Center(child: IconButton(onPressed: (){}, icon: Icons.restart,),)
           const Spacer(),
         ],
       ),
