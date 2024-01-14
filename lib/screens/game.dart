c5ab485e25ab111b217049c61c4f6f9dee9b30ca
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-15 00:57:16 +0100

    commit

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -384,7 +384,8 @@ class _GameScreenState extends State<GameScreen> {
     int? fillEmptySpace(List row, int rowIndex) {
       for (int columnIndex = 0; columnIndex < row.length - 3; columnIndex++) {
         if (row[columnIndex] == 0) {
-          if (columnIndex > 0 || columnIndex < row.length - 3) {
+          if ((columnIndex > 0 || columnIndex < row.length - 3) &&
+              hasSupport(rowIndex, columnIndex)) {
             if (row[columnIndex + 1] == targetNumber &&
                 row[columnIndex + 2] == targetNumber) {
               return columnIndex;
