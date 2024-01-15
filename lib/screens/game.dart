096d28b02259f12df2515421fc4710c8b9025055
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-15 23:51:46 +0100

    commit

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -148,23 +148,125 @@ class _GameScreenState extends State<GameScreen> {
                           backgroundBlendMode: BlendMode
                               .dstOut), // This one will handle background + difference out
                     ),
-                    Align(
-                      alignment: Alignment.topCenter,
+                    Center(
                       child: Container(
-                        margin: const EdgeInsets.only(top: 80),
-                        height: 200,
-                        width: 200,
+                        // height: (30 * (6 + 2)).w,
+                        // width: (30 * (7 + 2)).w,
+                        padding: EdgeInsets.all(2.5.w),
                         decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(10.r),
                         ),
-                      ),
-                    ),
-                    const Center(
-                      child: Text(
-                        'Hello World',
-                        style: TextStyle(
-                            fontSize: 70, fontWeight: FontWeight.w600),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: List.generate(
+                            7,
+                            (rowIndex) => Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: List.generate(
+                                7,
+                                (columnIndex) {
+                                  Color? color;
+                                  if (gameBoard[rowIndex][columnIndex] != 0) {
+                                    if (gameBoard[rowIndex][columnIndex] == 1) {
+                                      color = Colors.red;
+                                    } else if (gameBoard[rowIndex]
+                                            [columnIndex] ==
+                                        2) {
+                                      color = Colors.yellow;
+                                    }
+                                  }
+                                  return GestureDetector(
+                                    onTap: () async {
+                                      if (canPlay && !computerIsPlaying) {
+                                        makeMove(columnIndex);
+                                        if (canPlay) {
+                                          setState(() {
+                                            computerIsPlaying = true;
+                                          });
+                                          int computerMove =
+                                              await player2.play();
+                                          makeMove(computerMove);
+                                          setState(() {
+                                            computerIsPlaying = false;
+                                          });
+                                        }
+                                      }
+                                    },
+                                    child: (winningPositions.contains(
+                                            Position(rowIndex, columnIndex)))
+                                        ? Container(
+                                            width: 35.w,
+                                            height: 35.w,
+                                            margin: EdgeInsets.all(5.w),
+                                            decoration: BoxDecoration(
+                                              color: color,
+                                              shape: BoxShape.circle,
+                                              gradient: gameBoard[rowIndex]
+                                                          [columnIndex] ==
+                                                      0
+                                                  ? LinearGradient(
+                                                      colors: [
+                                                        Colors.deepPurple[400]!,
+                                                        backgroundColor!,
+                                                      ],
+                                                      begin:
+                                                          Alignment.topCenter,
+                                                      end: Alignment
+                                                          .bottomCenter,
+                                                    )
+                                                  : null,
+                                              border: Border.all(
+                                                color: Colors.white,
+                                                width: 3.sp,
+                                              ),
+                                            ),
+                                          )
+                                            .animate()
+                                            .scaleXY(
+                                              begin: 1,
+                                              end: 1.2,
+                                              duration: 500.milliseconds,
+                                              delay: 100.milliseconds,
+                                              curve: Curves.easeInOutQuart,
+                                            )
+                                            .then()
+                                            .scaleXY(
+                                              begin: 1,
+                                              end: .833333333333333333,
+                                              duration: 300.milliseconds,
+                                              delay: 500.milliseconds,
+                                              curve: Curves.bounceOut,
+                                            )
+                                        : Container(
+                                            width: 35.w,
+                                            height: 35.w,
+                                            margin: EdgeInsets.all(5.w),
+                                            decoration: BoxDecoration(
+                                              color: color,
+                                              shape: BoxShape.circle,
+                                              gradient: gameBoard[rowIndex]
+                                                          [columnIndex] ==
+                                                      0
+                                                  ? LinearGradient(
+                                                      colors: [
+                                                        Colors.deepPurple[400]!,
+                                                        backgroundColor!,
+                                                      ],
+                                                      begin:
+                                                          Alignment.topCenter,
+                                                      end: Alignment
+                                                          .bottomCenter,
+                                                    )
+                                                  : null,
+                                            ),
+                                          ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
                     )
                   ],
