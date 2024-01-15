113047e7c9d6a02b707df9b29c5c3105e1f9958e
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-15 22:38:48 +0100

    commit

--- lib/classes/computer_player.dart
+++ lib/classes/computer_player.dart
@@ -1,6 +1,8 @@
 import 'package:connect4/classes/player.dart';
 import 'package:connect4/classes/position.dart';
 import 'package:connect4/data.dart';
+import 'package:http/http.dart' as http;
+
 
 class ComputerPlayer extends Player {
   Position? lastPlayedPosition;
@@ -9,7 +11,18 @@ class ComputerPlayer extends Player {
 
   String boardToString() => gameBoard.map((row) => row.join()).join();
 
-  int play() {
+  Future<int> play() async {
+    var client = http.Client();
+try {
+  var response = await client.post(
+      Uri.https('example.com', 'whatsit/create'),
+      body: {'name': 'doodle', 'color': 'blue'});
+  var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+  var uri = Uri.parse(decodedResponse['uri'] as String);
+  print(await client.get(uri));
+} finally {
+  client.close();
+}
     return 0;
   }
 }
