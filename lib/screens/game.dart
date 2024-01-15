f30843b3808581d42e574ff40eb652952c1c85b6
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-15 02:50:45 +0100

    commit

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -249,13 +249,13 @@ class _GameScreenState extends State<GameScreen> {
 
         if (row[columnIndex] == currentPlayerInt) {
           positions.add(Position(rowIndex, columnIndex));
-          if (positions.size() == 4) {
+          
+        } else {if (positions.size() >= 4) {
             return {
               'winner': currentPlayerInt,
               'positions': positions,
             };
           }
-        } else {
           positions.clear();
           positions.add(Position(rowIndex, columnIndex));
           currentPlayerInt = row[columnIndex];
