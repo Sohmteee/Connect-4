411b7532f7964e551ca121fe979ba5e14e115424
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-15 23:01:15 +0100

    commit

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -114,10 +114,10 @@ class _GameScreenState extends State<GameScreen> {
                           onTap: () {
                             if (canPlay) {
                               makeMove(columnIndex);
-                              Future.delayed(600.milliseconds, () {
+                              Future.delayed(600.milliseconds, () async {
                                 if (canPlay) {
-                                  // makeMove(player2.play());
-                                  player2.play();
+                                  int computerMove = await player2.play();
+                                  makeMove(computerMove);
                                 }
                               });
                             }
