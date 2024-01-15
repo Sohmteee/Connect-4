7c889b074389897937b810468e870a0650759342
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-15 01:26:35 +0100

    commit

--- lib/classes/computer_player.dart
+++ lib/classes/computer_player.dart
@@ -1,13 +1,18 @@
 import 'package:connect4/classes/player.dart';
+import 'package:connect4/data.dart';
 
-class ComputerPlayer extends Player{
+class ComputerPlayer extends Player {
   ComputerPlayer(super.number);
 
-  void play() {
+  void play() {}
 
+  /// Method to check if the cell has support from the bottom
+  bool hasSupport(int rowIndex, int columnIndex) {
+    return (rowIndex == 5) ? true : gameBoard[rowIndex + 1][columnIndex] != 0;
   }
 
+
   void completeThree() {
-    
+    // horizon
   }
 }
--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -374,42 +374,12 @@ class _GameScreenState extends State<GameScreen> {
     return {};
   }
 
-  /// Method to check if the cell has support from the bottom
-  bool hasSupport(int rowIndex, int columnIndex) {
-    return (rowIndex == 5) ? true : gameBoard[rowIndex + 1][columnIndex] != 0;
-  }
+
 
   void horizontalMove(int playerNumber, int targetNumber) {
     getBoundaries(List row, int index) {}
 
-    /// Method to place a disk into an empty space either between two disks of
-    /// the target color or at the beginning/end of a row if the next disk
-    /// has the target color.
-    int? fillEmptySpace(List row, int rowIndex) {
-      for (int columnIndex = 0; columnIndex < row.length - 3; columnIndex++) {
-        if (row[columnIndex] == 0) {
-          if ((columnIndex > 0 || columnIndex < row.length - 3) &&
-              hasSupport(rowIndex, columnIndex)) {
-            if (row[columnIndex + 1] == targetNumber &&
-                row[columnIndex + 2] == targetNumber) {
-              return columnIndex;
-            }
-          }
-          if (columnIndex == 0 && hasSupport(rowIndex, columnIndex)) {
-            if (row[columnIndex + 1] == targetNumber) {
-              return columnIndex;
-            }
-          }
-          if (columnIndex == row.length - 3 &&
-              hasSupport(rowIndex, columnIndex)) {
-            if (row[columnIndex - 1] == targetNumber) {
-              return columnIndex;
-            }
-          }
-        }
-      }
-      return null;
-    }
+ 
 
     for (int rowIndex = 5; rowIndex >= 0; rowIndex--) {
       List row = gameBoard[rowIndex];
