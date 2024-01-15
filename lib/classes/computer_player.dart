42547d10963e43f9d962844c60a4aa91cbda695f
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-15 01:50:56 +0100

    commit

--- lib/classes/computer_player.dart
+++ lib/classes/computer_player.dart
@@ -42,34 +42,7 @@ class ComputerPlayer extends Player {
   }
 
   void completeHorizontalThree(int targetNumber) {
-    /// Method to place a disk into an empty space either between two disks of
-    /// the target color or at the beginning/end of a row if the next disk
-    /// has the target color.
-    int? fillEmptySpace(List row, int rowIndex) {
-      for (int columnIndex = 0; columnIndex < row.length - 3; columnIndex++) {
-        if (row[columnIndex] == 0) {
-          if ((columnIndex > 0 || columnIndex < row.length - 3) &&
-              hasSupport(rowIndex, columnIndex)) {
-            if (row[columnIndex + 1] == targetNumber &&
-                row[columnIndex + 2] == targetNumber) {
-              return columnIndex;
-            }
-          }
-          if (columnIndex == 0 && hasSupport(rowIndex, columnIndex)) {
-            if (row[columnIndex + 1] == targetNumber) {
-              return columnIndex;
-            }
-          }
-          if (columnIndex == row.length - 3 &&
-              hasSupport(rowIndex, columnIndex)) {
-            if (row[columnIndex - 1] == targetNumber) {
-              return columnIndex;
-            }
-          }
-        }
-      }
-      return null;
-    }
+    
 
     getBoundaries(List row, int index) {}
 
@@ -86,5 +59,34 @@ class ComputerPlayer extends Player {
       }
       // complete a pair
     }
+  }
+
+  /// Method to place a disk into an empty space either between two disks of
+  /// the target color or at the beginning/end of a row if the next disk
+  /// has the target color.
+  int? fillEmptySpace(List row, int rowIndex) {
+    for (int columnIndex = 0; columnIndex < row.length - 3; columnIndex++) {
+      if (row[columnIndex] == 0) {
+        if ((columnIndex > 0 || columnIndex < row.length - 3) &&
+            hasSupport(rowIndex, columnIndex)) {
+          if (row[columnIndex + 1] == targetNumber &&
+              row[columnIndex + 2] == targetNumber) {
+            return columnIndex;
+          }
+        }
+        if (columnIndex == 0 && hasSupport(rowIndex, columnIndex)) {
+          if (row[columnIndex + 1] == targetNumber) {
+            return columnIndex;
+          }
+        }
+        if (columnIndex == row.length - 3 &&
+            hasSupport(rowIndex, columnIndex)) {
+          if (row[columnIndex - 1] == targetNumber) {
+            return columnIndex;
+          }
+        }
+      }
+    }
+    return null;
   }
 }
