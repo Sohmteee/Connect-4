8c8846663cde2452e72a2fa7885b1e9aca786a65
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-15 01:52:29 +0100

    commit

--- lib/classes/computer_player.dart
+++ lib/classes/computer_player.dart
@@ -7,7 +7,6 @@ class ComputerPlayer extends Player {
   int humanPlayerNumber;
   ComputerPlayer(super.number, {required this.humanPlayerNumber});
 
-
   void play() {
     // try to win horizontally
 
@@ -22,6 +21,9 @@ class ComputerPlayer extends Player {
     // try to block any win diagonally
 
     // try to fill in an empty space horizontally
+    if (fillEmptySpace() != null) {
+      // place disk
+    }
 
     // try to complete a pair vertically
 
@@ -42,8 +44,6 @@ class ComputerPlayer extends Player {
   }
 
   void completeHorizontalThree(int targetNumber) {
-    
-
     getBoundaries(List row, int index) {}
 
     for (int rowIndex = 5; rowIndex >= 0; rowIndex--) {
@@ -53,10 +53,6 @@ class ComputerPlayer extends Player {
 
       // complete a triple
 
-      // fill in an empty space
-      if (fillEmptySpace(row, rowIndex) != null) {
-        // place disk
-      }
       // complete a pair
     }
   }
@@ -64,25 +60,28 @@ class ComputerPlayer extends Player {
   /// Method to place a disk into an empty space either between two disks of
   /// the target color or at the beginning/end of a row if the next disk
   /// has the target color.
-  int? fillEmptySpace(List row, int rowIndex) {
-    for (int columnIndex = 0; columnIndex < row.length - 3; columnIndex++) {
-      if (row[columnIndex] == 0) {
-        if ((columnIndex > 0 || columnIndex < row.length - 3) &&
-            hasSupport(rowIndex, columnIndex)) {
-          if (row[columnIndex + 1] == targetNumber &&
-              row[columnIndex + 2] == targetNumber) {
-            return columnIndex;
+  int? fillEmptySpace(List row, int rowIndex, int targetNumber) {
+    for (int rowIndex = 5; rowIndex >= 0; rowIndex--) {
+      List row = gameBoard[rowIndex];
+      for (int columnIndex = 0; columnIndex < row.length - 3; columnIndex++) {
+        if (row[columnIndex] == 0) {
+          if ((columnIndex > 0 || columnIndex < row.length - 3) &&
+              hasSupport(rowIndex, columnIndex)) {
+            if (row[columnIndex + 1] == targetNumber &&
+                row[columnIndex + 2] == targetNumber) {
+              return columnIndex;
+            }
           }
-        }
-        if (columnIndex == 0 && hasSupport(rowIndex, columnIndex)) {
-          if (row[columnIndex + 1] == targetNumber) {
-            return columnIndex;
+          if (columnIndex == 0 && hasSupport(rowIndex, columnIndex)) {
+            if (row[columnIndex + 1] == targetNumber) {
+              return columnIndex;
+            }
           }
-        }
-        if (columnIndex == row.length - 3 &&
-            hasSupport(rowIndex, columnIndex)) {
-          if (row[columnIndex - 1] == targetNumber) {
-            return columnIndex;
+          if (columnIndex == row.length - 3 &&
+              hasSupport(rowIndex, columnIndex)) {
+            if (row[columnIndex - 1] == targetNumber) {
+              return columnIndex;
+            }
           }
         }
       }
