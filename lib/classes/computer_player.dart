a0c4b830a4974654a673602c6e501916b78680c4
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-16 19:09:49 +0100

    commit

--- lib/classes/computer_player.dart
+++ lib/classes/computer_player.dart
@@ -58,7 +58,6 @@ class ComputerPlayer extends Player {
       // return (play());
       debugPrint('Error: $e');
       debugPrint('Playing offline...');
-      debugPrint(boardToString().length.toString());
       return offlinePlay();
     } finally {
       client.close();
