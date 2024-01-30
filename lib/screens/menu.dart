e9eaa76513a1f130e4f2af3960e64fce6adbbb14
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-30 05:33:21 +0100

    commit

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MenuScreen extends StatefulWidget {
   const MenuScreen({super.key});
@@ -11,14 +13,46 @@ class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  Center(
+      body: Center(
         child: Stack(
           alignment: Alignment.center,
           children: [
             buildBoard(),
             buildDiscs(),
-            buildTapHighlight(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Container buildBoard() {
+    return Container(
+      padding: EdgeInsets.all(2.5.w),
+      decoration: BoxDecoration(
+        color: Colors.deepPurple,
+        borderRadius: BorderRadius.circular(10.r),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(
+          7,
+          (rowIndex) => Row(
+            mainAxisSize: MainAxisSize.min,
+            children: List.generate(
+              7,
+              (columnIndex) {
+                return Container(
+                  width: 35.w,
+                  height: 35.w,
+                  margin: EdgeInsets.all(5.w),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.deepPurple[700],
+                  ),
+                );
+              },
+            ),
+          ),
         ),
       ),
     );
