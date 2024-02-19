c8c27b925621b3d59c033684126bf4696183fa3b
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-19 05:55:56 +0100

    commit

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 class GameButton extends StatelessWidget {
-  const GameButton({super.key});
+  const GameButton({super.key, this.text, this.onPressed});
+
+  
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ZoomTapAnimation(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 15.w),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/game button.png'),
+          ),
+        ),
+        child: Center(
+          child: Text(
+            'Play ',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18.sp,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
