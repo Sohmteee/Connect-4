49f84d6f5cf1fec391c1a6a7d2b175f9440a40c7
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-20 08:56:16 +0100

    commit

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -32,7 +32,7 @@ class GameScreen extends StatefulWidget {
 
 class _GameScreenState extends State<GameScreen> {
   final player1 = Player(1, name: 'You');
-  final player2 = widget.gameMode ==  ComputerPlayer(2, humanPlayerNumber: 1, name: 'Nora');
+  late final Player player2;
   int? winner;
   late Player currentPlayer;
   bool isGameOver = false;
@@ -125,6 +125,8 @@ class _GameScreenState extends State<GameScreen> {
   @override
   void initState() {
     super.initState();
+    player2 = widget.gameMode == GameMode.singlePlayer
+        ComputerPlayer(2, humanPlayerNumber: 1, name: 'Nora'): Player(2, name: 'Player 2');
     firstPlayer = player1;
     currentPlayer = firstPlayer;
     reset();
