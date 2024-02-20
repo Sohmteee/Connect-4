3ed5c185918b690ea6edec9ffdf6c41eed0b7a91
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-20 18:30:44 +0100

    commit

--- lib/providers/settings.dart
+++ lib/providers/settings.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class SettingsProvider extends ChangeNotifier {
   bool music = false;
   bool sound = false;
+  double volume = 0.5;
 
   void toggleMusic(bool value) {
     music = value;
