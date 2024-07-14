f96faf74aa4c1fd24ab5bc2a927c9784fff51c78
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-07-14 08:49:37 +0100

    commit

--- lib/screens/rooms/create.dart
+++ lib/screens/rooms/create.dart
@@ -101,7 +101,43 @@ class _CreateRoomScreenState extends State<CreateRoomScreen> {
                   return room.doc(roomKey).get().then((doc) => doc.exists);
                 }
 
-                if ()
+                if (roomName.text.isEmpty) {
+                   showDialog(
+                    context: context,
+                    builder: (context) {
+                      return Dialog(
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(10.w, 20.h, 0.w, 20.h),
+                          decoration: const BoxDecoration(),
+                          child: Text(
+                            'Room name is empty!',
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      );
+                    });
+                } else if (roomKey.text.isEmpty) {
+                   showDialog(
+                    context: context,
+                    builder: (context) {
+                      return Dialog(
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(10.w, 20.h, 0.w, 20.h),
+                          decoration: const BoxDecoration(),
+                          child: Text(
+                            'Room already exists!',
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      );
+                    });
+                }
 
                 if (await roomsContains(roomKey.text)) {
                   showDialog(
